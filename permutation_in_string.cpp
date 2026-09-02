ee8327ef1c4514151b341b38fc0dd889fdf56736
WAGHCHAURE OM <158573023+989077@users.noreply.github.com>
2026-09-02 22:48:09 +0530

    Add files via upload
    permutation in string

---
+++ permutation_in_string.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    bool isFreqSame(int freq1[], int freq2[]){
+        for(int i=0; i<26; i++){
+            if(freq1[i] != freq2[i]){
+                return false;
+            }
+        }
+        return true;
+    }
+    bool checkInclusion(string s1, string s2) {
+        int freq[26] ={0};
+        for(int i=0; i<s1.length(); i++){
+            freq[s1[i] - 'a']++;
+        }
+        int windSize = s1.length();
+        for(int i=0; i<s2.length(); i++){
+            int windIdx = 0, Idx =i;
+            int windFreq[26] ={0};
+            while(windIdx < windSize && Idx < s2.length()){
+                windFreq[s2[Idx] - 'a']++;
+                windIdx++;
+                Idx++;
+
+            }
+            if(isFreqSame(freq, windFreq)){
+                return true;
+            }
+        }
+        return false;
+    }
+};
+int main(){
+	string s1 = "ab" ;
+	string s2 = "eidbaooo" ;
+	Solution Sol;
+	bool result =  Sol.checkInclusion(s1,  s2);
+	cout<<"output:-"<<result<<endl;
+
+	return 0;
+}
+
