d30b8b75c0bec94406e897ff37cb85d048fe51c5
WAGHCHAURE OM <158573023+989077@users.noreply.github.com>
2026-09-14 22:57:09 +0530

    findding using duplicate_hashMap

---
+++ duplicate_hashMap285.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0], fast= nums[0];
+         
+       do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+
+
+       }while(slow != fast);
+
+       slow = nums[0];
+
+       while(slow != fast){
+        slow = nums[slow];
+        fast = nums[fast];
+       }
+       return slow;
+    }
+};
+int main(){
+	Solution sol;
+	vector<int> nums = {1,3,4,2,2};
+	int result = sol.findDuplicate(nums);
+	cout<<"duplicate:-"<<result<<" ";
+	
+	cout<<endl;
+	return 0;
+}
